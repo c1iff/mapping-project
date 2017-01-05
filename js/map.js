557b31c8767121bd8c0de6f81e7f8c3e4b4a11d6
Chris Clifford and Ryan Peterson <ryan@>
2017-01-05 14:57:07 -0800

    refactor map.js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,17 @@
+var results = require('./../007-locations.json')
 
 function Map() {
 }
 
+var input;
 var map;
 var infoWindow;
 var service;
 var heatmap;
+var markers = [];
 
 Map.prototype.initMap = function (json) {
+  var instance = this;
   var styledMapType = new google.maps.StyledMapType(
     json, {name: 'Styled Map'});
 
@@ -19,117 +23,163 @@ Map.prototype.initMap = function (json) {
         'styled_map']
       }
     });
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 
     var types = document.getElementById('type-selector');
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
 
+    instance.searchBar();
+
+    $('#create-marker').click(function(){
+      instance.addMarker();
+    });
     //*************Code for autocomplete search bar***********
     // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length === 0) {
-        return;
-      }
-
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        // Create a marker for each place.
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
     //   //************************************
 
-      map.mapTypes.set('styled_map', styledMapType);
-      map.setMapTypeId('styled_map');
 
     //*************geojson*************
     $('#changetype-heat').click(function() {
-      // heatmap.setMap(heatmap.getMap() ? null : map);
-      var results = require('./../007-locations.json')
-      //eqfeed_callback(json)
-      var heatmapData = [];
-      for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1], coords[0]);
-        heatmapData.push(latLng);
-      }
-
-      heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData,
-        dissipating: false,
-        map: map
-      });
+      instance.clearLayers();
+      instance.createHeatMap()
     })
+
     $('#changetype-marker').click(function() {
-      map.data.loadGeoJson('007-locations.json')
+      instance.clearLayers();
+      instance.createJson();
     })
 
+    $('#changetype-all').click(function() {
+      instance.clearLayers();
+      instance.showAll()
+    })
 
-  //*************geojson*************
-
-
-
-
-    $('#create-marker').click(function(){
-      var latitude = parseFloat($('#latitude').val());
-      var longitude = parseFloat($('#longitude').val());
-      var marker = new google.maps.Marker({
-        position: {lat: latitude, lng: longitude},
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        map: map
-      });
-    });
-
-    // function eqfeed_callback(results) {
-    // }
 };
+
+Map.prototype.clearLayers = function () {
+
+  for (var i = 0; i < markers.length; i++) {
+    map.data.remove(markers[i]);
+  }
+  if (heatmap) {
+    heatmap.setMap(null);
+  }
+};
+
+Map.prototype.createHeatMap = function(){
+  var instance = this;
+  // instance.clearLayers();
+  var heatmapData = [];
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    heatmapData.push(latLng);
+  }
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map
+  });
+}
+
+Map.prototype.createJson = function () {
+  var instance = this;
+  // instance.clearLayers();
+  markers = map.data.addGeoJson(results);
+};
+
+Map.prototype.showAll = function() {
+  var instance = this;
+  // instance.clearLayers();
+  var heatmapData = [];
+  for (var i = 0; i < results.features.length; i++) {
+    var coords = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coords[1], coords[0]);
+    heatmapData.push(latLng);
+  }
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    dissipating: false,
+    map: map
+  });
+
+  markers = map.data.addGeoJson(results);
+}
+
+Map.prototype.addMarker = function(){
+
+  var latitude = parseFloat($('#latitude').val());
+  var longitude = parseFloat($('#longitude').val());
+  var marker = new google.maps.Marker({
+    position: {lat: latitude, lng: longitude},
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    map: map
+  });
+}
+
+Map.prototype.searchBar = function(){
+
+  input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
 
 
 exports.mapModule = Map;
