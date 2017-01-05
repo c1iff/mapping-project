c707c11ebdd801495d1d3ea494f12d1662b40367
Chris Clifford and Ryan Peterson <chris@>
2017-01-05 11:29:28 -0800

    add data viszualization for location data

--- js/map.js
+++ js/map.js
@@ -1,6 +1,11 @@
+
 function Map() {
 }
+
 var map;
+var infoWindow;
+var service;
+
 Map.prototype.initMap = function (json) {
   var styledMapType = new google.maps.StyledMapType(
     json, {name: 'Styled Map'});
@@ -14,9 +19,91 @@ Map.prototype.initMap = function (json) {
       }
     });
 
-    // loadKmlLayer(src, map);
-    map.mapTypes.set('styled_map', styledMapType);
-    map.setMapTypeId('styled_map');
+    // //*************Code for autocomplete search bar***********
+    // // Create the search box and link it to the UI element.
+    // var input = document.getElementById('pac-input');
+    // var searchBox = new google.maps.places.SearchBox(input);
+    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    //
+    // // Bias the SearchBox results towards current map's viewport.
+    // map.addListener('bounds_changed', function() {
+    //   searchBox.setBounds(map.getBounds());
+    // });
+    //
+    // var markers = [];
+    // // Listen for the event fired when the user selects a prediction and retrieve
+    // // more details for that place.
+    // searchBox.addListener('places_changed', function() {
+    //   var places = searchBox.getPlaces();
+    //
+    //   if (places.length === 0) {
+    //     return;
+    //   }
+    //
+    //   // Clear out the old markers.
+    //   markers.forEach(function(marker) {
+    //     marker.setMap(null);
+    //   });
+    //   markers = [];
+    //
+    //   // For each place, get the icon, name and location.
+    //   var bounds = new google.maps.LatLngBounds();
+    //   places.forEach(function(place) {
+    //     if (!place.geometry) {
+    //       console.log("Returned place contains no geometry");
+    //       return;
+    //     }
+    //     var icon = {
+    //       url: place.icon,
+    //       size: new google.maps.Size(71, 71),
+    //       origin: new google.maps.Point(0, 0),
+    //       anchor: new google.maps.Point(17, 34),
+    //       scaledSize: new google.maps.Size(25, 25)
+    //     };
+    //
+    //     // Create a marker for each place.
+    //     markers.push(new google.maps.Marker({
+    //       map: map,
+    //       icon: icon,
+    //       title: place.name,
+    //       position: place.geometry.location
+    //     }));
+    //
+    //     if (place.geometry.viewport) {
+    //       // Only geocodes have viewport.
+    //       bounds.union(place.geometry.viewport);
+    //     } else {
+    //       bounds.extend(place.geometry.location);
+    //     }
+    //   });
+    //   map.fitBounds(bounds);
+    // });
+    //   //************************************
+
+      map.mapTypes.set('styled_map', styledMapType);
+      map.setMapTypeId('styled_map');
+
+    //*************geojson*************
+    map.data.loadGeoJson('007-locations.json')
+    var results = require('./../007-locations.json')
+    //eqfeed_callback(json)
+    var heatmapData = [];
+    for (var i = 0; i < results.features.length; i++) {
+      var coords = results.features[i].geometry.coordinates;
+      var latLng = new google.maps.LatLng(coords[1], coords[0]);
+      heatmapData.push(latLng);
+    }
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData,
+      dissipating: false,
+      map: map
+    });
+
+
+  //*************geojson*************
+
+
+
 
     $('#create-marker').click(function(){
       var latitude = parseFloat($('#latitude').val());
@@ -29,23 +116,9 @@ Map.prototype.initMap = function (json) {
       });
     });
 
-    var script = document.createElement('script');
-    // This example uses a local copy of the GeoJSON stored at
-    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    document.getElementsByTagName('head')[0].appendChild(script);
+    // function eqfeed_callback(results) {
+    // }
 };
-// Loop through the results array and place a marker for each
-// set of coordinates.
-window.eqfeed_callback = function(results) {
-  for (var i = 0; i < results.features.length; i++) {
-    var coords = results.features[i].geometry.coordinates;
-    var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    var marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-  }
-}
+
 
 exports.mapModule = Map;
